d11b9665cf0297352872e652d8b3315ab3c4d9f3
Reivnaj <pelletierjanvier@gmail.com>
2020-04-21 23:14:31 +0200

    Create a fortnite like game with José et Josianne

--- app.rb
+++ app.rb
@@ -1,7 +1,39 @@
+require 'pry'
 require 'bundler'
 Bundler.require
 
 require_relative 'lib/game'
 require_relative 'lib/player'
 
+player1 = Player.new("Josiane")
+player2 = Player.new("José")
+
+puts "A ma droite #{player1.name}"
+puts "A ma gauche #{player2.name}"
+
+
+while player1.life_points>0 || player2.life_points>0
+  puts "=============================="
+  puts "Voici l'état de nos joueurs :"
+  player1.show_state
+  player2.show_state
+  puts "=============================="
+  puts "Passons à la phase d'attaque :"
+  puts "==!==!==!==!==!==!==!==!==!==!==!==!==!==!=="
+  player1.attacks(player2)
+  if player2.life_points <= 0
+    break
+  end
+  player2.show_state
+  player2.attacks(player1)
+  if player1.life_points <= 0
+    break
+  end
+  player1.show_state
+
+end
+
+
+
 binding.pry
+puts "end of game"
--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,47 @@
 class Player
+  attr_accessor :name, :life_points
 
+  def initialize(name, life_points=10)
+    @name = name
+    @life_points = life_points
+  end
 
+  def show_state
 
+      if  @life_points>0 && @life_points<5
+        puts "#{@name} est mal-en-point, il ne lui reste que #{@life_points} points de vie!"
+      else
+        puts "#{@name} a #{@life_points} points de vie"
+      end
+
+  end
+
+  def gets_damage(damage_value)
+    @life_points -= damage_value
+    if @life_points <= 0
+      puts "#{@name} est décédé(e) des suites de la fracture du crâne que vous lui avez affligé"
+    end
+  end
+
+  def attacks(player_attacked)
+    puts "#{@name} attaque #{player_attacked.name} !"
+    damage = comptute_damage
+    player_attacked.life_points -= damage
+    puts "==!==!==!==!==!==!==!==!==!==!=="
+    puts "#{player_attacked.name} reçoit #{damage} points de dégat"
+  end
+
+  def comptute_damage
+    return rand(1..6)
+  end
 end
+
+# class HumanPlayer < Player
+#   attr_accessor :weapon_level
+#
+#   def initialize(name, weapon_level, life_points=100)
+#     weapon_level=1
+#     @weapon_level = weapon_level
+#     super(name, life_points=10)
+#   end
+# end
